1fec68d8e7423d69650b7fa342305525bc29a0e0
Ivcota <ivcotad@gmail.com>
2022-04-14 20:32:56 -0700

    not working commits

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const AnyComponent = Component as any;
+
+  return (
+    <MantineProvider withGlobalStyles>
+      <AnyComponent {...pageProps} />
+    </MantineProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/app/check-in.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CheckIn = () => {
+  return <div>check-in</div>;
+};
+
+export default CheckIn;
